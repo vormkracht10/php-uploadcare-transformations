cb80edeae9d281a406fb04ba3b45144b8a6dba95
Bas van Dinther <bas@vormkracht10.nl>
2022-07-07 09:12:26 +0200

    Color enum

---
+++ src/Transformations/Enums/Color.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Vormkracht10\UploadcareTransformations\Transformations\Enums;
+
+enum Color
+{
+    case BRIGHTNESS;
+    case EXPOSURE;
+    case GAMMA;
+    case CONTRAST;
+    case SATURATION;
+    case VIBRANCE;
+    case WARMTH;
+}
