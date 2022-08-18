bd548d106ef94c764ed28c917e8e4ebb7ef75633
Bas van Dinther <bas@vormkracht10.nl>
2022-08-18 07:49:48 +0200

    Rename Methods trait to Validations

--- src/Transformations/SmartCrop.php
+++ src/Transformations/SmartCrop.php
@@ -2,14 +2,14 @@
 
 namespace Vormkracht10\UploadcareTransformations\Transformations;
 
-use Vormkracht10\UploadcareTransformations\Traits\Methods;
+use Vormkracht10\UploadcareTransformations\Traits\Validations;
 use Vormkracht10\UploadcareTransformations\Transformations\Enums\CropType;
 use Vormkracht10\UploadcareTransformations\Transformations\Enums\Offset;
 use Vormkracht10\UploadcareTransformations\Transformations\Interfaces\TransformationInterface;
 
 class SmartCrop implements TransformationInterface
 {
-    use Methods;
+    use Validations;
 
     public const WIDTH = 'width';
     public const HEIGHT = 'height';
