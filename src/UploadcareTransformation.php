b9bf91e577f6b1a5508404a027a2b3f062203b21
Bas van Dinther <bas@vormkracht10.nl>
2022-10-17 08:17:45 +0200

    Signed url (wip)

--- src/UploadcareTransformation.php
+++ src/UploadcareTransformation.php
@@ -10,11 +10,24 @@ class UploadcareTransformation extends Transformations
     protected string $url;
     protected string $baseUrl;
     protected ?string $filename = null;
+    protected ?string $timestamp = null;
+    protected ?string $acl = null;
+    protected ?string $token = null;
 
-    public function __construct(string $uuid, string $cdnUrl = 'https://ucarecdn.com/')
-    {
+    public function __construct(
+        string $uuid, 
+        string $cdnUrl = 'https://ucarecdn.com/', 
+        string|null $timestamp = null,
+        string|null $acl = null,
+        string|null $token = null,
+    ){
         $this->uuid = $uuid;
         $this->baseUrl = $cdnUrl;
+
+        $this->timestamp = $timestamp;
+        $this->acl = $acl;
+        $this->token = $token;
+
     }
 
     public function filename(string $filename): string
@@ -22,6 +35,13 @@ class UploadcareTransformation extends Transformations
         $this->filename = $filename;
 
         return $this;
+    }
+
+    public function getSignedUrl(): string
+    {
+        $url = $this->getUrl();
+
+        return $url . '/?token=exp=' . $this->timestamp . '~acl=' . $this->acl . '~hmac=' . $this->token;
     }
 
     public function getUrl(): string
